a388ce32fe37c3da576083d5abdd56ce1977d237
postromantic83 <Agile.brain83@gmail.com>
2015-08-06 20:42:07 +0300

    added view

---
+++ src/main/java/view/ControlPanel.java
@@ -0,0 +1,42 @@
+package view;
+
+import control.Crane;
+
+import javax.swing.*;
+
+/**
+ * Created by sokolov on 16.02.2015.
+ */
+public class ControlPanel extends JPanel {
+
+   private Crane crane;
+
+    ControlPanel (){
+
+
+    }
+
+    @Override
+    public String toString() {
+        return "CONTROL_PANEL";
+    }
+
+    public  void setControl(ControlPanel lp) {
+
+        System.out.println("CONTROL_PANEL setControl - method!");
+
+    };
+
+    //getters for sections
+    public int getS120HC_25 (){ return 0;}
+    public int getS120HC_50 (){ return 0;}
+    public int getS154HC_168 (){ return 0;}
+    public int getS154HC_1616 (){ return 0;}
+    public int getS120HC_125() {return 0;}
+    public int getS132HC_168() {return 0;}
+
+
+
+
+
+}
---
+++ src/main/java/view/PanelAction.java
@@ -0,0 +1,106 @@
+package view;
+
+import control.Crane;
+import control.EverdigmCrane;
+import control.LiebherrCrane;
+import control.PotainCrane;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by Sokolov on 03.02.2015.
+ *
+ *
+ *
+ */
+public class PanelAction implements ActionListener {
+
+    CranePanel cranePanel;
+    Crane crane;
+    JPanel panControl;
+    ControlPanel controlPanel;
+
+    public PanelAction(CranePanel cranePanel, ControlPanel controlPanel) {
+
+        System.out.println ("PanelAction - constr: " + controlPanel.toString());
+        this.cranePanel = cranePanel;
+        this.controlPanel = controlPanel;
+
+        System.out.println("this.controlPanel: : " + this.controlPanel.toString());
+
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent actionEvent) {
+        Object src = actionEvent.getSource();
+        int val1;
+
+
+
+        System.out.println("controlPanel: " + controlPanel.toString());
+
+
+
+        //set customer
+       if (src==cranePanel.comb1) {
+
+           if (cranePanel.comb1.getSelectedItem() == "Liebherr") {
+
+               crane = new LiebherrCrane(cranePanel);
+
+               }
+           else if (cranePanel.comb1.getSelectedItem() == "Potain") {
+
+               crane = new PotainCrane(cranePanel);
+
+           }
+           else if (cranePanel.comb1.getSelectedItem() == "Everdigm") {
+
+               crane = new EverdigmCrane(cranePanel);
+
+           }
+           //clean and fill comb3 from crane class
+           cranePanel.comb2.removeAllItems();
+           cranePanel.setComb2(crane.getAvailableModels());
+           }
+
+
+
+
+
+
+        if (src==cranePanel.comb2){
+
+
+
+
+            //clean and fill comb3 from crane class
+            cranePanel.comb3.removeAllItems();
+            cranePanel.setComb3(crane.getAvailableModifications((String) cranePanel.comb2.getSelectedItem()));
+            crane.setModel((String) cranePanel.comb2.getSelectedItem());
+            crane.createControl(cranePanel, crane);
+
+
+
+            }
+
+
+
+        prepareCrane();
+
+        }
+
+        public void prepareCrane (){
+
+
+            System.out.println("setTowerCombination method!");
+
+
+        }
+
+
+    }
+
