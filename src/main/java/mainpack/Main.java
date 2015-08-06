5bbbec96d1e23f1b1838492bd32a64233416f2cc
postromantic83 <Agile.brain83@gmail.com>
2015-08-06 09:47:42 +0300

    some comments

--- src/main/java/mainpack/Main.java
+++ src/main/java/mainpack/Main.java
@@ -16,6 +16,7 @@ public class Main {
     public static void main(String[] args) {
 
 
+        //just now only towercranea, then abstractcrain will be extended for other cranes
 
         TowerCrane crane = new TowerCrane();
         CranePanel cranePanel = new CranePanel();
